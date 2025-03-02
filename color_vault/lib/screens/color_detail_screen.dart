4e97ab9f9badc03d7cebfba1f700519d3ee532ab
GaneshManickam <ganeshmanickam0@gmail.com>
2025-03-02 19:30:19 +0530

    cleanup

--- color_vault/lib/screens/color_detail_screen.dart
+++ color_vault/lib/screens/color_detail_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hive/hive.dart';
-import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 
 class ColorDetailScreen extends StatefulWidget {
   final Color color;
